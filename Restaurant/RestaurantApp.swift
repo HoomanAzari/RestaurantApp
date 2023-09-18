4ce149462c6db0e3b0f4087fae3b213ba239175b
Houman <veryhouman@gmail.com>
2023-09-18 15:36:23 -0400

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Houman Azari on 2023-09-18.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
